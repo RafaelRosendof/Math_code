3ee291011a74a471ec293f73e38f9f19aef3a45c
rafael <rafael.19.rosendo@gmail.com>
2024-04-08 21:33:10 -0300

    corrigido

--- bissection.cpp
+++ bissection.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <cmath>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+namespace mp = boost::multiprecision;  
+
 
 double function(double x) {
     return x * x * x + cos(x);
 }
 
-double bisection(double a, double b, double error) {
+
+
+mp::cpp_dec_float_50 bisection(double a, double b, mp::cpp_dec_float_50 error) {
+
     int reps = 0;
     if (function(a) * function(b) > 0) {
         throw "Intervalo não possui raizes!!!\n";
     }
     int iter = 0; // Initialize iter
     while ((b - a) / 2 > error) {
-        double c = (a + b) / 2;
-        if (std::abs(function(c)) < error) { // Check if c is close to 0
+      double c = (a + b) / 2;
+        if (fabs(function(c)) < error) { // Check if c is close to 0
             std::cout << "Achou a raiz !!!";
             return c;
         } else if (function(c) * function(a) < 0) {
@@ -25,19 +32,20 @@ double bisection(double a, double b, double error) {
         reps++;
         iter++; // Increment iter
     }
+
     return (a + b) / 2;
 }
 
 int main() {
     std::cout << "Digite o erro: ";
-    double error;
+    mp::cpp_dec_float_50 error;
     std::cin >> error;
     std::cout << "Digite o intervalo: ";
     double a, b;
     std::cin >> a >> b;
 
     try {
-        double root = bisection(a, b, error);
+    mp::cpp_dec_float_50 root = bisection(a, b, error);
         std::cout << "Raiz encontrada: " << root << std::endl;
     } catch (const char* msg) {
         std::cerr << "Erro: " << msg << std::endl;
