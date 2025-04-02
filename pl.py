ef840e9e1edfd8e00b5d6fc41d1578a173c9a59e
Rafael Rosendo <rafael.rosendo.faustino.702@ufrn.edu.br>
2025-04-02 10:30:20 -0300

    pl tutorial

---
+++ pl.py
@@ -0,0 +1,44 @@
+from torch import optim , nn , utils , Tensor 
+from torchvision.datasets import MNIST 
+from torchvision.transforms import ToTensor 
+import lightning as L 
+import os 
+
+encoder = nn.Sequential(nn.Linear(28 * 28 , 64) nn.Relu() , nn.Linear(64,3))
+decode = nn.Sequential(nn.Linear(28 * 28 , 64) nn.Relu() , nn.Linear(64,3))
+
+class LitEncoder(L.LightningModule):
+    def __init__(self , encoder , decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        
+        
+    def training_step(self, batch , batch_idx):
+        x , _ = batch 
+        x = x.view(x.size(0) , -1)
+        z = self.encoder(x)
+        x2 = self.decoder(z) 
+        
+        
+        loss = nn.functional.mse_loss(x2 , x)
+        
+        self.log("Perca do treinamento", loss)
+        
+        return loss 
+    
+    
+    def config_optim(self):
+        optimi = optim.Adam(self.parameters() , lr=1e-2)
+        return optimi
+    
+    
+autoencoder = LitEncoder(encoder , decode)
+
+data = MNIST(os.getcwd() , download=True , transform=ToTensor())
+train_loader = utils.data.DataLoader(data)
+
+trainer = L.Trainer(limit_train_batches=1000 , max_epochs=1200)
+
+trainer.fit(model = autoencoder , train_dataloaders=train_loader)
+
